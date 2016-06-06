12e048039895e55b84e7c7ed3c2cd789d91913af
saulocamposi <saulocamposi@gmail.com>
2016-06-06 13:35:39 -0300

    Commented typescript compiler

--- src/system.config.js
+++ src/system.config.js
@@ -9,8 +9,8 @@
     '@angular':                   'https://npmcdn.com/@angular', // sufficient if we didn't pin the version
     'angular2-in-memory-web-api': 'https://npmcdn.com/angular2-in-memory-web-api', // get latest
     'rxjs':                       'https://npmcdn.com/rxjs@5.0.0-beta.6',
-    'ts':                         'https://npmcdn.com/plugin-typescript@4.0.10/lib/plugin.js',
-    'typescript':                 'https://npmcdn.com/typescript@1.8.10/lib/typescript.js',
+    //'ts':                         'https://npmcdn.com/plugin-typescript@4.0.10/lib/plugin.js',
+    //'typescript':                 'https://npmcdn.com/typescript@1.8.10/lib/typescript.js',
  };
 
   //packages tells the System loader how to load when no filename and/or no extension
